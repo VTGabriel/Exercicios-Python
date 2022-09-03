2395d92cbaa4bd08c600cb65a9ef67f7e162e74d
Gabriel Thomaz <gabrielvthomaz@gmail.com>
2022-09-02 23:43:34 -0300

    Ex8

---
+++ Ex8.py
@@ -0,0 +1,3 @@
+salario_hora = float(input("Digite se salário por hora: "))
+horas_trabalho = float(input("Digite quantas horas de trabalho ao mês: "))
+print ("Seu salário no mês é",salario_hora*horas_trabalho)
